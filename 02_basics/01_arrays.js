fda6ad9b302bf11c37766c626576a2e1653146bd
Harsh Kumar Jangir <harshkumarjangir2002@gmail.com>
2025-01-05 22:38:06 +0530

    adding javascript basics -- arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,28 @@
+// Array
+
+// const myArr = [0, 1, 2, 3, 4, 5, "harsh", true]
+const myArr = [0, 1, 2, 3, 4, 5]
+const myLaptop = ["asus", "hp", "dell", "lenevo"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+
+// console.log(myArr);
+console.log(myArr[0]);
+console.log(myArr[4]);
+
+
+// Array methods
+
+// myArr.push(6) // Add element to array
+// myArr.push(7)
+// myArr.pop() // Remove the last element / value of array
+
+// myArr.unshift(5) // Insert an elemnet in starting on an array
+// myArr.shift() // Removes the starting element of an array
+
+console.log(myArr.includes(8)); // Ckeck if it exits in array
+console.log(myArr.indexOf(7));
+
+
+
+console.log(myArr);
